a83a3483d5e5e10790e9cc038932f3e93ba3f4f9
Vitor Pavan <vipavani@hotmail.com>
2021-09-06 18:53:09 +0200

    react app with material-ui and login page

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import { isAuthenticated } from './actions/auth';
+// import Dashboard from './pages/Dashboard/index';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/login' component={Login} />
+        {/* <Route exact path='/signup' component={Signup} />
+        <Route exact path='/forget-password' component={ForgetPassword} />
+        <Route
+          path='/reset-password/:resetPasswordToken'
+          component={ResetPassword}
+        /> */}
+        {/* <PrivateRoute exact path='/dashboard' component={Dashboard} />
+        <PrivateRoute exact path='/dashboard/*' component={Dashboard} /> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
